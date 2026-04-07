be39fcc217ad3efd814ca85c8e7aae78feb5f4e5
jaymaurya-tech <jaymaurya3103@gmail.com>
2026-04-07 10:04:43 +0530

    fix internal server

--- jay_traders/settings.py
+++ jay_traders/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = 'home'
